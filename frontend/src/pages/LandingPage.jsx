5d4b1d5baa783d25a2ee6b94c2f8d1d9a3a42b32
Sonu Kumar <sv808494@gmail.com>
2025-07-16 11:06:57 +0530

    Navbar component added

---
+++ frontend/src/pages/LandingPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+const LandingPage = () => {
+    return (
+        <div>
+            <Navbar />
+            This is the landing page
+        </div>
+    )
+}
+
+export default LandingPage
