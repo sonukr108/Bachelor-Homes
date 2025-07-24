5e00cba19f31a72a83f966638348180ccac51fed
Sonu Kumar <sv808494@gmail.com>
2025-07-24 23:47:31 +0530

    lading page working

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import LandingPage from './pages/LandingPage'
+import AboutUs from './pages/AboutUs'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/about" element={<AboutUs />} />
       </Routes>
     </BrowserRouter>
   )
